67257639c616656bbae0ce9a4d68c6e5ea4113b0
Jansen Junior <jsd.alcantara@gmail.com>
2020-07-19 23:09:55 -0400

    added basic cli command

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,40 @@
-use std::{process, fs};
+use std::{process, fs, env};
 use std::path::Path;
 
-const EXIT_CANNOT_EXECUTE: i32 = 126;
+enum ExitCodes {
+    General = 1,
+    CannotExecute = 126
+}
 
 fn main() {
-    let current_path = Path::new(".");
-    if let Err(_) = check_symlink(current_path) {
+    let mut args = env::args();
+    if args.len() > 2 {
+        println!("Something went wrong! Multiple directories are not supported yet");
+        process::exit(ExitCodes::CannotExecute as i32);
+    }
+    
+    let str_path = if args.len() == 1 { String::from(".") } 
+    else { 
+        match args.nth(1) {
+            Some(path) => path,
+            None => {
+                println!("Something went wrong! Could not get the first arg correctly!");
+                process::exit(ExitCodes::General as i32);
+            }
+        }
+    };
+
+    let path = Path::new(&str_path);
+    if let Err(_) = check_symlink(path) {
         println!("Something went wrong! Symlinks are not supported yet");
-        process::exit(EXIT_CANNOT_EXECUTE);
+        process::exit(ExitCodes::CannotExecute as i32);
     }
 
-    let metadata = match current_path.metadata() {
+    let metadata = match path.metadata() {
         Ok(meta) => meta,
         Err(e) => {
-            println!("Something went wrong! {}", e);
-            process::exit(EXIT_CANNOT_EXECUTE);
+            println!("Something went wrong for '{}'! {}", path.display(), e);
+            process::exit(ExitCodes::CannotExecute as i32);
         }
     };
 
