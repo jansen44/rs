564a1a52dd127a7bb7a9f215bea9cba31cd6e3f8
Jansen Junior <jsd.alcantara@gmail.com>
2020-07-20 22:37:46 -0400

    Added lib/lib.rs for RsApp struct

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,50 +1,7 @@
-use std::{process, fs, env};
-use std::path::Path;
-
-enum ExitCodes {
-    Success = 0,
-    CannotExecute = 126
-}
+use rs::RsApp;
 
 fn main() {
-    let mut args = env::args();
-    if args.len() > 2 {
-        println!("Something went wrong! Multiple directories are not supported yet");
-        process::exit(ExitCodes::CannotExecute as i32);
-    }
-    
-    let str_path = if args.len() == 1 { String::from(".") } 
-    else { args.nth(1).expect("Could not get the first arg correctly") };
-
-    let path = Path::new(&str_path);
-    if let Err(_) = check_symlink(path) {
-        println!("Something went wrong! Symlinks are not supported yet");
-        process::exit(ExitCodes::CannotExecute as i32);
-    }
-
-    let metadata = match path.metadata() {
-        Ok(meta) => meta,
-        Err(e) => {
-            println!("Something went wrong for '{}'! {}", path.display(), e);
-            process::exit(ExitCodes::CannotExecute as i32);
-        }
-    };
-
-    if metadata.is_file() {
-        println!("{}", path.display());
-        process::exit(ExitCodes::Success as i32);
-    }
-    
-    for entry in fs::read_dir(path).expect("Can't get file list") {
-        print!("{} ", entry.unwrap().path().display());
-    }
-    println!("");
-}
-
-fn check_symlink(path: &Path) -> Result<(), ()> {
-    match fs::read_link(path) {
-        Err(_) => Ok(()),
-        _ => Err(())
-    }
+    let app = RsApp::new();
+    app.run();
 }
 
---
+++ src/lib/lib.rs
@@ -0,0 +1,58 @@
+use std::{env, process, path, fs};
+
+pub enum ExitCodes {
+    Success = 0,
+    CannotExecute = 126
+}
+
+pub struct RsApp {
+    path: path::PathBuf,
+    metadata: fs::Metadata
+}
+
+impl RsApp {
+    pub fn new() -> RsApp {
+        let mut args = env::args();
+        // Temp: Handle args quantity
+        if args.len() > 2 {
+            println!("Something went wrong! Multiple directories are not supported yet");
+            process::exit(ExitCodes::CannotExecute as i32);
+        }
+
+        let path = get_path(&mut args);
+        let metadata = path.metadata().unwrap_or_else(|err| {
+            println!("Something went wrong for '{}'! {}", path.display(), err);
+            process::exit(ExitCodes::CannotExecute as i32);
+        });
+
+        RsApp { path, metadata }
+    }
+
+    pub fn run(&self) {
+        if self.metadata.is_file() {
+            println!("{}\n", self.path.display());
+            self.success();
+        }
+
+        for entry in fs::read_dir(&self.path).expect("Can't get file list") {
+            print!("{} ", entry.unwrap().path().display());
+        }
+        println!("");
+    }
+
+    fn success(&self) {
+        process::exit(ExitCodes::Success as i32);
+    }
+}
+
+
+fn get_path(args: &mut env::Args) -> path::PathBuf {
+    let str_path = args.nth(1).unwrap_or(String::from("."));
+    let path = path::PathBuf::from(&str_path);
+    if let Ok(_) = fs::read_link(&path) {
+        println!("Something went wrong! SymLinks are not supported yet");
+        process::exit(ExitCodes::CannotExecute as i32);
+    }
+    path
+}
+
