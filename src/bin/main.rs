74b81f7deafc9cdff73e8d0a40bd36197236aa1c
Jansen Junior <jsd.alcantara@gmail.com>
2020-07-26 18:48:01 -0400

    cowmand structs String => &str ⚡️

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,29 +1,29 @@
 use rs::{RsApp, Cowmand, Arg};
 
 fn main() {
-    let cow = Cowmand::new(String::from("rs"))
-        .description(String::from("A zero-dependency 'ls' alternative."))
+    let cow = Cowmand::new("rs")
+        .description("A zero-dependency 'ls' alternative.")
         .arg(
             Arg::new()
                 .short_command('l')
-                .long_command(String::from("list"))
-                .help(String::from("show files/directories in list mode."))
+                .long_command("list")
+                .help("show files/directories in list mode.")
         )
         .arg(
             Arg::new()
                 .short_command('a')
-                .long_command(String::from("all"))
-                .help(String::from("show all files/directories."))
+                .long_command("all")
+                .help("show all files/directories.")
         )
         .arg(
             Arg::new()
                 .short_command('h')
-                .long_command(String::from("help"))
-                .help(String::from("show this help message."))
+                .long_command("help")
+                .help("show this help message.")
         );
                 
     println!("{}", cow);
-    // let app = RsApp::new();
-    // app.run();
+    let app = RsApp::new();
+    app.run();
 }
 
--- src/lib/cowmand.rs
+++ src/lib/cowmand.rs
@@ -1,14 +1,13 @@
 use std::fmt;
 
-// Todo: Change 'String' to &str wherever it's possible
 #[derive(Default)]
-pub struct Arg {
+pub struct Arg<'arg> {
     short_command: Option<char>,
-    help: Option<String>,
-    long_command: Option<String>
+    help: Option<&'arg str>,
+    long_command: Option<&'arg str>
 }
 
-impl Arg {
+impl<'arg> Arg<'arg> {
     pub fn new() -> Self {
         Default::default()
     }
@@ -18,28 +17,27 @@ impl Arg {
         self
     }
 
-    pub fn long_command(mut self, long: String) -> Self {
+    pub fn long_command(mut self, long: &'arg str) -> Self {
         self.long_command = Some(long);
         self
     }
 
-    pub fn help(mut self, help: String) -> Self {
+    pub fn help(mut self, help: &'arg str) -> Self {
         self.help = Some(help);
         self
     }
 }
 
-// Todo: Change 'String' to &str wherever it's possible
 #[derive(Default)]
-pub struct Cowmand {
-    name: String,
-    description: Option<String>,
-    args: Vec<Arg>
+pub struct Cowmand<'cow> {
+    name: &'cow str,
+    description: Option<&'cow str>,
+    args: Vec<Arg<'cow>>
 }
 
-impl fmt::Display for Cowmand {
+impl<'cow> fmt::Display for Cowmand<'cow> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut message = self.name.clone();
+        let mut message = String::from(self.name);
         if let Some(d) = &self.description {
             message.push_str(&format!(" - {}", d));
         }
@@ -62,20 +60,20 @@ impl fmt::Display for Cowmand {
     }
 }
 
-impl Cowmand {
-    pub fn new(name: String) -> Self {
+impl<'cow> Cowmand<'cow> {
+    pub fn new(name: &'cow str) -> Self {
         Cowmand {
             name: name,
             ..Default::default()
         }
     }
 
-    pub fn description(mut self, description: String) -> Self {
+    pub fn description(mut self, description: &'cow str) -> Self {
         self.description = Some(description);
         self
     }
 
-    pub fn arg(mut self, arg: Arg) -> Self {
+    pub fn arg(mut self, arg: Arg<'static>) -> Self {
         self.args.push(arg);
         self
     }
