6322caa8e00ecd3c9ec22f9fa39060d22e8cd80a
Jansen Junior <jsd.alcantara@gmail.com>
2020-09-06 23:29:59 -0400

    :truck: Removed nested lib dirs

--- src/lib/cowmand.rs
+++ src/lib/cowmand.rs
@@ -1,7 +1,33 @@
-pub mod args;
-
 use std::{fmt, env};
-pub use args::Arg;
+
+#[derive(Default, Debug, Clone)]
+pub struct Arg<'arg> {
+	pub name: &'arg str,
+	pub short_command: Option<char>,
+	pub help: Option<&'arg str>,
+	pub long_command: Option<&'arg str>
+}
+
+impl<'arg> Arg<'arg> {
+	pub fn new(name: &'arg str) -> Self {
+		Arg {
+			name: name,
+			..Default::default()
+		}
+	}
+
+	pub fn short_command(mut self, short: char) -> Self {
+		self.short_command = Some(short); self
+	}
+
+	pub fn long_command(mut self, long: &'arg str) -> Self {
+		self.long_command = Some(long); self
+	}
+
+	pub fn help(mut self, help: &'arg str) -> Self {
+		self.help = Some(help); self
+	}
+}
 
 trait CowmandVector {
 	fn try_insert_uniq(&mut self, wrapped_name: Option<String>);
